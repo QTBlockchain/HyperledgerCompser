2fb1a55d1e44cd84cb75d49e0499160323d97be4
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2018-03-24 19:46:15 +0530

    Added election business network

---
+++ March18/Samples/qt-study-election/lib/logic.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {org.qt.hyperledger.samples.election.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('org.qt.hyperledger.samples.election.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
